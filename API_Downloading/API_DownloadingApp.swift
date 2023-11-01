e3f7647a7510c744692927ba99ad10bab68b17c7
Sachin <sachinsharma2168@gmail.com>
2023-11-01 16:13:29 +0530

    Initial Commit

---
+++ API_Downloading/API_DownloadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_DownloadingApp.swift
+//  API_Downloading
+//
+//  Created by Sachin Sharma on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct API_DownloadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API_Downloading/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API_Downloading
+//
+//  Created by Sachin Sharma on 01/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
